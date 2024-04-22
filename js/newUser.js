5205bb6212393d9439a569155a92d6dce34db5a2
ivan <soyivan@gmail.com>
2024-04-22 17:52:29 -0300

    Feat: Add save to local storage

--- js/newUser.js
+++ js/newUser.js
@@ -20,10 +20,43 @@ form.addEventListener('submit', (e) => {
         const password = document.getElementById('password').value;
         alert(`User create succesfully. The date is Username: ${username}, Email: ${email} and Password: ${password}`);
 
+        saveToLocalStorage()
+
         window.history.back();
-        }
+    }
     catch (error) {
         alert("You made a mistake. Please try again.");
         console.log(error);
     }
-    })
+})
+
+function saveToLocalStorage() {
+    let newEmail = document.getElementById('email').value
+    let newUsername = document.getElementById('username').value
+    let newPassword = document.getElementById('password').value
+
+    if (localStorage.getItem('email') == null) {
+        localStorage.setItem('email', '[]' )
+    }
+
+    if (localStorage.getItem('username') == null) {
+        localStorage.setItem('username', '[]' )
+    } 
+
+    if (localStorage.getItem('password') == null) {
+        localStorage.setItem('password', '[]' )
+    }
+
+    let oldEmail = JSON.parse(localStorage.getItem('email'))
+    oldEmail.push(newEmail)
+
+    let oldUsername = JSON.parse(localStorage.getItem('username'))
+    oldUsername.push(newUsername)
+
+    let oldPassword = JSON.parse(localStorage.getItem('password'))
+    oldPassword.push(newPassword)
+
+    localStorage.setItem('email', JSON.stringify(oldEmail));
+    localStorage.setItem('username', JSON.stringify(oldUsername));
+    localStorage.setItem('password', JSON.stringify(oldPassword));
+}

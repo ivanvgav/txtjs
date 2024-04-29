3b65d17c49a0471159c796faf7ec86572bf1c17f
ivan <soyivan@gmail.com>
2024-04-28 21:28:51 -0300

    Trying to make checker run, but I failed
    Made modification to cdn for crypto-js, add fuction to check password,
    but failed. The returns values are `undefinied`.

--- js/modules/checkUsernamePassword.js
+++ js/modules/checkUsernamePassword.js
@@ -3,14 +3,23 @@ import { encryptPassword } from './encryptPassword.js'
 
 export function checkUserAndPassword(username, password) {
     const savedUsers = Array(getUsers()); 
-    let checkUser = savedUsers.find((user) => {
+    let checkUsername = savedUsers.find((user) => {
         return username == user.username
     });
-    console.log(checkUser)
+    console.log(checkUsername);
     
-    if (checkUser == undefined) {
-        alert("This is not a created username. Please create a new account")
-        console.log("This is not a created username. Please create a new account")
+    if (checkUsername == undefined) {
+        alert("This is not a created username. Please create a new account");
+        console.log("This is not a created username. Please create a new account");
     }
 
+    let checkUserPassword = savedUsers.find((user) => {
+        encryptPassword(password)
+        return password == user.password
+    });
+    console.log(checkUserPassword);
+
+    if (checkUserPassword == undefined) {
+        console.log("This is not a created password. Please create a new account");
+    }
 }

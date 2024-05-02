acc6a0272c8ccdfba9d2faf9bc62b3996b55cbf6
ivan <soyivan@gmail.com>
2024-05-02 10:12:24 -0300

    Tasks page and new task form

---
+++ js/tasks.js
@@ -0,0 +1,11 @@
+import { appearBtn, closeBtn, hideBtn } from "./modules/login.js" 
+
+window.appearBtn = function() {
+    appearBtn()
+}
+window.closeBtn = function() {
+    closeBtn()
+}
+window.hideBtn = function() {
+    hideBtn()
+}
